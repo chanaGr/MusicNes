c17e174746862d300a9a379fe89bad1e0c6140f1
chanaGr <106763060+chanaGr@users.noreply.github.com>
2022-07-10 03:07:06 +0300

    night

--- src/data.tsx
+++ src/data.tsx
@@ -135,37 +135,37 @@ export class Manager {
 //=================================
  const productsList = new ProductsList();
   
-    productsList.addProduct("German Side Flute", 1, 4, 1240, 12, "picturs/Germansideflute.jpg");
-    productsList.addProduct("Organic", 2, 1, 3240,40, "picturs/Organic.jpg");
-    productsList.addProduct("Germany Violin", 3, 3, 8240, 3, "picturs/Germanviolin.jpg");
-    productsList.addProduct("Alone Drum", 4, 2, 210, 23, "picturs/Alonedrum.jpg");
-    productsList.addProduct("Electric Piano", 5, 1, 7929, 14, "picturs/Electricpiano.jpg");
-    productsList.addProduct("Ancient Manual Cymbals", 6, 2, 2000, 12, "picturs/Ancientmanualcymbals.jpg"); 
-    productsList.addProduct("German Drumset", 7, 2, 9700, 40, "picturs/Germandrumset.jpg");
-    productsList.addProduct("Darbuka", 8, 2, 67, 23, "picturs/Darbuka.jpg");
-    productsList.addProduct("Cymbals Pillar", 9, 2, 990, 88, "picturs/CymbalsPillar.jpg");
-    productsList.addProduct("Clarinet", 10, 4, 2400, 70, "picturs/Clarinet.jpg");
-    productsList.addProduct("Manual Cymbals", 11, 2, 3200, 1, "picturs/Manualcymbals.jpg");
-    productsList.addProduct("ancient Villain", 12, 3, 6740, 70, "picturs/ancientvillain.jpg");
-    productsList.addProduct("Bells", 13, 2, 1240, 70, "picturs/Bells.jpg");
-    productsList.addProduct("Ancient Drumset", 14, 2, 2400, 70, "picturs/Ancientdrumset.jpg");
-    productsList.addProduct("Grand Piano", 15, 1, 1120, 3, "picturs/Grandpiano.jpg");
-    productsList.addProduct("Saxophone", 16, 4, 7840, 10, "picturs/saxophone.jpg");
-    productsList.addProduct("Wooden Gitar", 17, 3, 899, 88, "picturs/Woodenguitar.jpg");
-    productsList.addProduct("Shaped Villain", 18, 3, 2220, 70, "picturs/Shapedvillain.jpg");
-    productsList.addProduct("gold Vilion", 19, 3, 1999, 88, "picturs/goldvilion.jpg");
-    productsList.addProduct("Trombone", 20, 4, 2403, 70, "picturs/trombone.jpg");
-    productsList.addProduct("Designed Guitar", 21, 3, 240, 70, "picturs/Designedguitar.jpg");
-    productsList.addProduct("In Novative Drumset", 22, 2, 3539, 15, "picturs/Innovativedrumset.jpg");
-    productsList.addProduct("Contra Bass", 23, 3, 3399, 88, "picturs/ContraBass.jpg");
-    productsList.addProduct("Cello", 24, 3, 240, 70, "picturs/cello.jpg");
-    productsList.addProduct("Rose Gold Cymbals", 25, 2, 70242, 13, "picturs/RoseGoldcymbals.jpg");
-    productsList.addProduct("Ancient Violin", 26, 3, 7854, 88, "picturs/ancientviolin.jpg");
-    productsList.addProduct("Wooden Harmonica", 27, 4, 14356, 70, "picturs/Woodenharmonica.jpg");
-    productsList.addProduct("Piano", 28, 1, 10009, 90, "picturs/piano.jpg");
-    productsList.addProduct("Wooden Flute", 29, 4, 350, 70, "picturs/Woodenflute.jpg");
-    productsList.addProduct("Trumpet", 30, 4, 1240, 70, "picturs/trumpet.jpg");
-    productsList.addProduct("Mandoline",31, 3, 2240, 70, "picturs/mandoline.jpg");
+    productsList.addProduct("German Side Flute", 1, 4, 1240, 12, "/MusicNes/picturs/Germansideflute.jpg");
+    productsList.addProduct("Organic", 2, 1, 3240,40, "/MusicNes/picturs/Organic.jpg");
+    productsList.addProduct("Germany Violin", 3, 3, 8240, 3, "/MusicNes/picturs/Germanviolin.jpg");
+    productsList.addProduct("Alone Drum", 4, 2, 210, 23, "/MusicNes/picturs/Alonedrum.jpg");
+    productsList.addProduct("Electric Piano", 5, 1, 7929, 14, "/MusicNes/picturs/Electricpiano.jpg");
+    productsList.addProduct("Ancient Manual Cymbals", 6, 2, 2000, 12, "/MusicNes/picturs/Ancientmanualcymbals.jpg"); 
+    productsList.addProduct("German Drumset", 7, 2, 9700, 40, "/MusicNes/picturs/Germandrumset.jpg");
+    productsList.addProduct("Darbuka", 8, 2, 67, 23, "/MusicNes/picturs/Darbuka.jpg");
+    productsList.addProduct("Cymbals Pillar", 9, 2, 990, 88, "/MusicNes/picturs/CymbalsPillar.jpg");
+    productsList.addProduct("Clarinet", 10, 4, 2400, 70, "/MusicNes/picturs/Clarinet.jpg");
+    productsList.addProduct("Manual Cymbals", 11, 2, 3200, 1, "/MusicNes/picturs/Manualcymbals.jpg");
+    productsList.addProduct("ancient Villain", 12, 3, 6740, 70, "/MusicNes/picturs/ancientvillain.jpg");
+    productsList.addProduct("Bells", 13, 2, 1240, 70, "/MusicNes/picturs/Bells.jpg");
+    productsList.addProduct("Ancient Drumset", 14, 2, 2400, 70, "/MusicNes/picturs/Ancientdrumset.jpg");
+    productsList.addProduct("Grand Piano", 15, 1, 1120, 3, "/MusicNes/picturs/Grandpiano.jpg");
+    productsList.addProduct("Saxophone", 16, 4, 7840, 10, "/MusicNes/picturs/saxophone.jpg");
+    productsList.addProduct("Wooden Gitar", 17, 3, 899, 88, "/MusicNes/picturs/Woodenguitar.jpg");
+    productsList.addProduct("Shaped Villain", 18, 3, 2220, 70, "/MusicNes/picturs/Shapedvillain.jpg");
+    productsList.addProduct("gold Vilion", 19, 3, 1999, 88, "/MusicNes/picturs/goldvilion.jpg");
+    productsList.addProduct("Trombone", 20, 4, 2403, 70, "/MusicNes/picturs/trombone.jpg");
+    productsList.addProduct("Designed Guitar", 21, 3, 240, 70, "/MusicNes/picturs/Designedguitar.jpg");
+    productsList.addProduct("In Novative Drumset", 22, 2, 3539, 15, "/MusicNes/picturs/Innovativedrumset.jpg");
+    productsList.addProduct("Contra Bass", 23, 3, 3399, 88, "/MusicNes/picturs/ContraBass.jpg");
+    productsList.addProduct("Cello", 24, 3, 240, 70, "/MusicNes/picturs/cello.jpg");
+    productsList.addProduct("Rose Gold Cymbals", 25, 2, 70242, 13, "/MusicNes/picturs/RoseGoldcymbals.jpg");
+    productsList.addProduct("Ancient Violin", 26, 3, 7854, 88, "/MusicNes/picturs/ancientviolin.jpg");
+    productsList.addProduct("Wooden Harmonica", 27, 4, 14356, 70, "/MusicNes/picturs/Woodenharmonica.jpg");
+    productsList.addProduct("Piano", 28, 1, 10009, 90, "/MusicNes/picturs/piano.jpg");
+    productsList.addProduct("Wooden Flute", 29, 4, 350, 70, "/MusicNes/picturs/Woodenflute.jpg");
+    productsList.addProduct("Trumpet", 30, 4, 1240, 70, "/MusicNes/picturs/trumpet.jpg");
+    productsList.addProduct("Mandoline",31, 3, 2240, 70, "/MusicNes/picturs/mandoline.jpg");
  
   
 //const managers = new Manager(1234, "chani")
